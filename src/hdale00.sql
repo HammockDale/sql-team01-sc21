eca36c8d231a7016f4cc2a21af96e79ecce812b6
Ekaterina Ruseva <hdale@at-h3.msk.21-school.ru>
2023-06-11 05:22:49 +0300

    END

--- src/hdale00.sql
+++ src/hdale00.sql
@@ -1,17 +1,6 @@
 
-WITH
-t AS (
-	SELECT name, MAX(updated) AS max_updated
-	FROM currency
-	GROUP BY name),
-c AS (
-	SELECT cu.id, cu.name, cu.rate_to_usd, t.max_updated
-	FROM currency cu
-		JOIN t ON t.name = cu.name AND t.max_updated = cu.updated
-),
-
-
-
+WITH 
+c AS (SELECT * FROM currency WHERE updated IN (SELECT max(updated) FROM currency GROUP BY id)),
 n_t AS (SELECT DISTINCT
 	COALESCE(u.name, 'not defined') AS name, 
 	COALESCE(u.lastname, 'not defined') AS lastname, b.type, 
@@ -19,12 +8,12 @@ n_t AS (SELECT DISTINCT
 	CASE
 		WHEN COUNT(c.name)  OVER (PARTITION BY  u.id, b.type)  = 1  THEN c.name
 		ELSE 'not defined'
-	END  AS currency_name,
+	END AS currency_name,
  CASE
         WHEN COUNT(c.rate_to_usd)  OVER (PARTITION BY  u.id, b.type)  = 1  THEN c.rate_to_usd
         
         ELSE '1'
-    END  AS last_rate_to_usd
+    END AS last_rate_to_usd
 FROM balance b
     LEFT JOIN "user" u ON b.user_id = u.id
     LEFT JOIN c ON b.currency_id = c.id)
